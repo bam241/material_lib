066d995155395249787e132cf836679b20357251
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2019-03-19 09:23:58 -0500

    working as expected

--- build_rsim_mat_lib.cc
+++ build_rsim_mat_lib.cc
@@ -3,7 +3,7 @@
 
 int main() {
   pyne::MaterialLibrary g4_lib = build_g4_matlib();
-//g4_lib = set_namespace(g4_lib, "G4_NIST");
+  g4_lib = set_namespace(g4_lib, "G4_NIST");
 
   pyne::MaterialLibrary pyne_lib = pyne::MaterialLibrary();
 
@@ -11,7 +11,8 @@ int main() {
       "/Users/mouginot/.local/lib/python3.5/site-packages/pyne/nuc_data.h5",
       "/material_library/materials");
   pyne_lib = set_namespace(pyne_lib, "pyne");
+  
   pyne_lib.merge(g4_lib);
-
+  pyne::matname_set my_name_set = pyne_lib.get_matlist();
   pyne_lib.write_hdf5("full_lib.h5m");
 }
